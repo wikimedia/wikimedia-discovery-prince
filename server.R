dcaef19572bdc809782068f653e7720b18aba682
Mikhail Popov <mikhail@mpopov.com>
2016-01-25 13:51:50 -0800

    Reskins the entire dashboard to a light-on-dark theme
    The previous patch "inverted" the geo graph theme to
    make the colors (which were also changed by the patch
    to polloi) pop out better. This resulted in the new
    geo graph looking really out of place, and it clashed
    with the white bar at the top of the dashboard. Thus,
    this patch reskins the rest of the dashboard to that
    theme, making the geo graph appear in unison with the
    rest while also differentiating the Portal dashboard
    visually from the other Discovery dashboard, which is
    appropriate given the uniqueness of the project/team.
    
    Bug: T123959
    Change-Id: Id0ed69719b04f1d0590ed0e22ff459ef253a3638

--- server.R
+++ server.R
@@ -13,9 +13,9 @@ shinyServer(function(input, output){
   }
   
   output$clickthrough_rate_dygraph <- renderDygraph({
-    polloi::make_dygraph(
-      data = clickthrough_rate,
-      xlab = "Date", ylab = "Clickthrough rate (%)", title = "Wikipedia portal clickthrough rate") %>%
+    clickthrough_rate %>%
+      polloi::make_dygraph(xlab = "Date", ylab = "Clickthrough rate (%)", title = "Wikipedia portal clickthrough rate") %>%
+      dyCSS(css = "www/inverse.css") %>%
       dyAxis("x", axisLabelFormatter = polloi::custom_axis_formatter, axisLabelWidth = 70) %>%
       dyAnnotation(as.Date("2015-12-07"), text = "A",
                    tooltip = "Sampling change - see below",
@@ -26,6 +26,7 @@ shinyServer(function(input, output){
     polloi::make_dygraph(
       data = action_breakdown,
       xlab = "Date", ylab = "Actions (%)", title = "Actions on the Wikipedia portal") %>%
+      dyCSS(css = "www/inverse.css") %>%
       dyAxis("x", axisLabelFormatter = polloi::custom_axis_formatter, axisLabelWidth = 70) %>%
       dyLegend(labelsDiv = "action_breakdown_legend", show = "always", width = 400) %>%
       dyAnnotation(as.Date("2015-12-07"), text = "A",
@@ -37,6 +38,7 @@ shinyServer(function(input, output){
     polloi::make_dygraph(
       data = dwelltime_data,
       xlab = "Date", ylab = "Dwell Time (Seconds)", title = "Time spent on the Wikipedia portal") %>%
+      dyCSS(css = "www/inverse.css") %>%
       dyAxis("x", axisLabelFormatter = polloi::custom_axis_formatter, axisLabelWidth = 70) %>%
       dyAnnotation(as.Date("2015-12-07"), text = "A",
                    tooltip = "Sampling change - see below",
@@ -46,8 +48,8 @@ shinyServer(function(input, output){
   output$country_breakdown_dygraph <- renderDygraph({
     country_data %>%
       polloi::make_dygraph(xlab = "", ylab = "Users (%)", title = "Geographic breakdown of portal visitors") %>%
+      dyCSS(css = "www/inverse.css") %>%
       dyAxis("x", axisLabelFormatter = polloi::custom_axis_formatter, axisLabelWidth = 70) %>%
-      dyLegend(labelsDiv = "country_breakdown_legend", show = "always", width = 400) %>%
-      dyCSS(css = "www/inverse.css")
+      dyLegend(labelsDiv = "country_breakdown_legend", show = "always", width = 400)
   })
 })
